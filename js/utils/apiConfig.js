e057d951bf533ac0c1895100824514775efe382a
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-12-17 11:14:39 +0530

    Update apiConfig.js

--- js/utils/apiConfig.js
+++ js/utils/apiConfig.js
@@ -1,9 +1,9 @@
 export const API_KEYS = {
   VISION_API: 'AIzaSyDnEZ6sNwbxVVVeQoTpEL22fep39b4I0oc',
-  GEMINI_API: 'AIzaSyA6crBKIIcjw6WbG-jaobiswZXnpxYJ0T4'
+  GEMINI_API: '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'
 };
 
 export const API_ENDPOINTS = {
   VISION: 'https://vision.googleapis.com/v1/images:annotate',
-  GEMINI: 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent'
+  GEMINI: 'https://api.groq.com/openai/v1/chat/completions'
 };
