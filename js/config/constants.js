1bbe8e59fd3a7a5c26a5c4e00b7599bf1cd83214
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-12-17 11:05:34 +0530

    Update constants.js

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,10 +1,10 @@
 export const API_KEYS = {
     VISION_API: 'AIzaSyDnEZ6sNwbxVVVeQoTpEL22fep39b4I0oc',
-    GEMINI_API: 'AIzaSyDtN9czLTfmVm3FvC-MzL3fdbqIiHMGRxo',
+    GEMINI_API: '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',
     TINYMCE_API: 'cg09wsf15duw9av3kj5g8d8fvsxvv3uver3a95xyfm1ngtq4'
 };
 
 export const API_ENDPOINTS = {
     VISION: 'https://vision.googleapis.com/v1/images:annotate',
-    GEMINI: 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent'
+    GEMINI: 'https://api.groq.com/openai/v1/chat/completions'
 };
