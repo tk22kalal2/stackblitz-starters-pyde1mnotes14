3761094158119f5a5a1bab30d7f81c0fcd2a46b2
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-12-17 16:23:20 +0530

    Update constants.js

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,5 +1,5 @@
 export const API_KEYS = {
-    VISION_API: 'AIzaSyAhPm8HdbFFkVg1v0tO_19T0VN05t478dE',
+    VISION_API: 'AIzaSyAkUca71ka6KhkRr7ftcO4awqBmK-uZO0c',
     GROQ_API: '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',
     TINYMCE_API: 'cg09wsf15duw9av3kj5g8d8fvsxvv3uver3a95xyfm1ngtq4'
 };
